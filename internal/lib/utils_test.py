32f7c587fea09c33cbf8f4e227317e46e0b0c958
herbertxue <herbertxue@google.com>
2021-05-03 15:43:12 +0800

    Remove the TCP limit from webrtc brower link.
    Bug: 187018262
    Test: acloud-dev create --autoconnect webrtc
    Change-Id: Id6667f3c75d12c43d9345c36adba0c4f65b4ec74

--- internal/lib/utils_test.py
+++ internal/lib/utils_test.py
@@ -481,7 +481,7 @@ class UtilsTest(driver_test_lib.BaseDriverTest):
                          "ip": "192.168.1.1",},],}
 
         utils.LaunchBrowserFromReport(fake_report)
-        webbrowser.open_new_tab.assert_called_once_with("https://localhost:8443/?use_tcp=true")
+        webbrowser.open_new_tab.assert_called_once_with("https://localhost:8443")
         webbrowser.open_new_tab.call_count = 0
 
         # test local instance
@@ -489,7 +489,7 @@ class UtilsTest(driver_test_lib.BaseDriverTest):
             "devices": [{"instance_name": "local-instance1",
                          "ip": "127.0.0.1:6250",},],}
         utils.LaunchBrowserFromReport(fake_report)
-        webbrowser.open_new_tab.assert_called_once_with("https://localhost:8443/?use_tcp=true")
+        webbrowser.open_new_tab.assert_called_once_with("https://localhost:8443")
         webbrowser.open_new_tab.call_count = 0
 
         # verify terminal can't support launch webbrowser.
